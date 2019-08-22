89cca59812b9acb7fbfa11344601517d7334dc6f
Nenad Nikolić <45372226+nenad014@users.noreply.github.com>
2019-08-22 20:19:07 +0200

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,4 @@
+<div class="jumbotron text-center">
+	<h1>Geometricus</h1>
+	<h3>Sajt za upoznavanje sa geometrijom</h3>
+</div>
---
+++ includes/menu.php
@@ -0,0 +1,17 @@
+<ul class="nav nav-pills nav-stacked">
+	<li><a href="index.php">Početna</a></li>
+	<li class="dropdown">
+		<a class="dropdown-toogle" data-toggle="dropdown" href="#">Figure<span class="caret"></span></a>
+			<ul class="dropdown-menu">
+				<li><a href="pravougaonik.php">Pravougaonik</a></li>
+				<li><a href="trougao.php">Trougao</a></li>
+				<li><a href="krug.php">Krug</a></li>
+				<li><a href="kvadrat.php">Kvadrat</a></li>
+				<li><a href="kocka.php">Kocka</a></li>
+				<li><a href="valjak.php">Valjak</a></li>
+				<li><a href="lopta.php">Lopta</a></li>
+				<li><a href="kupa.php">Kupa</a></li>
+				<li><a href="piramida.php">Piramida</a></li>
+			</ul>
+	</li>
+</ul>
