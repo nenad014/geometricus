ab142b88d2cbdfbb72349a991d61e62881f5cdc2
Nenad Nikolić <45372226+nenad014@users.noreply.github.com>
2019-08-22 20:18:28 +0200

    Update bottom.php

--- includes/bottom.php
+++ includes/bottom.php
@@ -1,1 +1,20 @@
+<!DOCTYPE html>
+<html>
 
+<head>
+	<style>
+		.bottom {
+			background-color: #2f2f2f;
+			padding: 10px;
+			color: #ffffff;
+		}
+	</style>
+</head>
+
+<body>
+	<div class="bottom">
+		<p class="text-center">geometricus &copy; <?php echo date('Y') ?> Sva prava zadržana. </p>
+		<p class="text-center">Developed by Nenad Nikolić</p>
+	</div>
+</body>
+</html>
